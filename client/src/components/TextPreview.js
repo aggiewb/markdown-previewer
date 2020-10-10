830bdea9cccebb0df5e3f89c5eb38af0b7b7ae83
AggieWB <usmaggie23@gmail.com>
2020-10-10 14:56:00 -0700

    Add props parameter, and user props.text value to p element
    contents

--- client/src/components/TextPreview.js
+++ client/src/components/TextPreview.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function TextPreview() {
+function TextPreview(props) {
     return <section>
-        <p id="preview"></p>
+        <p id="preview">{props.text}</p>
     </section>;
 }
 
