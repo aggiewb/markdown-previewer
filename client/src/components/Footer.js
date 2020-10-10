a7bdc8eb066e2ca4f61386a07259d02a710961e7
AggieWB <usmaggie23@gmail.com>
2020-10-10 14:58:29 -0700

    Change section element to footer, and add a tag element with
    contents

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Footer() {
-    return <section>
-
-    </section>;
+    return <footer>
+        <a href="https://www.aggiewb.com" rel="noreferrer noopener" target="_blank">Aggie Wheeler Bateman</a> &copy; {new Date().getFullYear()}
+    </footer>;
 }
 
 export default Footer;
