fb30b6948d058346507efc3039862bfd9f709fb3
AggieWB <usmaggie23@gmail.com>
2020-10-10 09:07:51 -0700

    Init a create react app project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+    }
+  }
+  
+  render(){
+  }
+}
+
+export default App;
