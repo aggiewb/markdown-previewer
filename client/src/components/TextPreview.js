85ec32d577556fde75d606f5efeacb172e03f1fc
AggieWB <usmaggie23@gmail.com>
2020-10-11 11:36:07 -0700

    Import marked module, set option of breaks to true to add
    <br> on a single line break, and set the dangerouslySetInnerHTML React property to the marked output
    Add commenting for documentation references
    Fulfill user stories #4 and Optional Bonus

--- client/src/components/TextPreview.js
+++ client/src/components/TextPreview.js
@@ -1,8 +1,15 @@
+//https://marked.js.org/
 import React from 'react';
+import marked from 'marked';
 
 function TextPreview(props) {
+    marked.setOptions({
+      breaks: true
+    });
+
+    //https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
     return <section>
-        <p id="preview">{props.text}</p>
+        <p id="preview" dangerouslySetInnerHTML={{__html: marked(props.text)}}></p>
     </section>;
 }
 
