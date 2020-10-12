69c48c086bd7250528f1ac4e259efff35551a2c8
AggieWB <usmaggie23@gmail.com>
2020-10-12 06:51:38 -0700

    Add a tag for image attribution, and wrap each a "section" in a
    div for styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Footer() {
     return <footer>
-        <a href="https://www.aggiewb.com" rel="noreferrer noopener" target="_blank">Aggie Wheeler Bateman</a> &copy; {new Date().getFullYear()}
+        <div>
+            Image by <a href="https://commons.wikimedia.org/wiki/File:Orange_cat_cartoon.png" rel="noreferrer noopener" target="_blank">XuLily</a>, licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.en" rel="noreferrer noopener" target="_blank">CC BY-SA 4.0</a>
+        </div>
+        <div>
+            <a href="https://www.aggiewb.com" rel="noreferrer noopener" target="_blank">Aggie Wheeler Bateman</a> &copy; {new Date().getFullYear()}
+        </div>
     </footer>;
 }
 
